ea909894490f9145bd62f6305cbe6956e789b060
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-02-03 22:08:33 -0500

    fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,23 +9,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-  size_t i, j, p_min;
-  int temporal;
+	size_t i, j, p_min;
+	int temporal;
 
-  for (i = 0; i < (size - 1); i++)
-    {
-      p_min = i;
-      for (j = i + 1; j < size; j++)
+	for (i = 0; i < (size - 1); i++)
 	{
-	  if (array[j] < array[p_min])
-	    p_min = j;
+		p_min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[p_min])
+				p_min = j;
+		}
+		if (p_min != i)
+		{
+			temporal = array[p_min];
+			array[p_min] = array[i];
+			array[i] = temporal;
+			print_array(array, size);
+		}
 	}
-      if (p_min != i)
-	{
-	  temporal = array[p_min];
-	  array[p_min] = array[i];
-	  array[i] = temporal;
-	  print_array(array, size);
-	}
-    }
 }
