1427ab90a89f0b141ba18adf361ca3d4c1fbc56c
Sebas15897 <sebastiancontreras15897@gmail.com>
2021-02-04 02:08:26 +0000

    update0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - This function sorts an array of integers in ascending
+ * order using the Bubble sort algorithm.
+ * @array: The array that is ordered.
+ * @size: The size of the array.
+ *
+ * Return: Nothing.
+ **/
+void bubble_sort(int *array, size_t size)
+{
+  size_t total_times, position_value;
+  int container = 0;
+
+  if (!array || !size || size < 2)
+    {
+      return;
+    }
+
+  total_times = 0;
+  while (total_times < size)
+    {
+      position_value = 0;
+      while (position_value < (size - 1))
+	{
+	  if (array[position_value] > array[position_value + 1])
+	    {
+	      container = array[position_value];
+	      array[position_value] = array[position_value + 1];
+	      array[position_value + 1] = container;
+	      print_array(array, size);
+	    }
+	  position_value++;
+	}
+      total_times++;
+    }
+}
---
+++ sort.h
@@ -0,0 +1,33 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+  const int n;
+  struct listint_s *prev;
+  struct listint_s *next;
+} listint_t;
+
+/* Prototype */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quicksort_recursion(int *array, size_t size, ssize_t first, ssize_t last);
+size_t partition(int *array, size_t size, ssize_t first, ssize_t last);
+void swap(int *array, size_t size, int *value1, int *value2);
+
+#endf
